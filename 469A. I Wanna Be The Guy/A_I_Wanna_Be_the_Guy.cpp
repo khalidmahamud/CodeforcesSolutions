765ec517ec0395a3be1c0c9052f3453be2ee6720
khalidmahamud <107807473+khalidmahamud@users.noreply.github.com>
2023-05-28 17:09:45 +0600

    updated 469A

--- 469A. I Wanna Be The Guy/A_I_Wanna_Be_the_Guy.cpp
+++ 469A. I Wanna Be The Guy/A_I_Wanna_Be_the_Guy.cpp
@@ -2,39 +2,33 @@
 #include <algorithm>
 using namespace std;
 
-void sortedArray (int arr[], int size) {
-    sort(arr, arr + size);
-}
 
 int main() {
-    int n, p;
+    int n, p, q;
     
     while (cin >> n) {
         cin >> p;
         int x[p];
-        int size1 = sizeof(x) / sizeof(x[0]);
 
         for (int i = 0; i < p; i++)
         {
             cin >> x[i];
         }
 
-        sortedArray(x, size1);
+        cin >> q;
+        int y[q];
 
-        cin >> p;
-        int y[p];
-        int size2 = sizeof(y) / sizeof(y[0]);
-
-        for (int i = 0; i < p; i++)
+        for (int i = 0; i < q; i++)
         {
             cin >> y[i];
         }
 
-        sortedArray(y, size2);
+        sort(x, x + p);
+        sort(y, y + q);
 
-        int merged[size1 + size2];
+        int merged[p + q];
 
-        int* it = set_union(x, x + size1, y, y + size2, merged);
+        int* it = set_union(x, x + p, y, y + q, merged);
         int mergedSize = it - merged;
 
         if (mergedSize == n) {
