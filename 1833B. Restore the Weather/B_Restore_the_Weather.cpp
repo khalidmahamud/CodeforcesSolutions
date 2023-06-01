cc34b1d027d2358704e2927dd2909190ee854c03
khalidmahamud <107807473+khalidmahamud@users.noreply.github.com>
2023-06-02 02:38:48 +0600

    solved 1833B

---
+++ 1833B. Restore the Weather/B_Restore_the_Weather.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    long t, n, k, diff;
+    cin >> t;
+
+    while (t--) {
+        cin >> n >> k;
+        long long a[n], b[n], ans[n];
+        vector<pair<long, long>> indices;
+        for (long i = 0; i < n; ++i) {
+            cin >> a[i];
+            indices.push_back(make_pair(a[i], i));
+        }
+        for (long i = 0; i < n; i++) {
+            cin >> b[i];
+        }
+        sort(indices.begin(), indices.end());
+        sort(b , b + n);
+
+        for (long i = 0; i < n; i++) {
+            ans[indices[i].second] = b[i];
+        }
+
+        for (long i = 0; i < n; i++) {
+            cout << ans[i] << " "; 
+        }
+        cout << endl;
+    }
+}
