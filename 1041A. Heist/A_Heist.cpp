7f5fac5232ac68e5f5fd1bcdda162191c86fe318
khalidmahamud <107807473+khalidmahamud@users.noreply.github.com>
2023-06-01 14:51:35 +0600

    solved 1041A

---
+++ 1041A. Heist/A_Heist.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i + 1] - a[i] > 1) {
+            count += a[i + 1] - a[i] - 1;
+        }
+    }
+    cout << count;
+}
